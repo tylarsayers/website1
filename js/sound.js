4b7f2c9dddf54caab15032a3e108a94e09a23ea3
tylarsayers <143061860+tylarsayers@users.noreply.github.com>
2024-03-05 22:31:49 +0000

    Tue, Mar 5, 2024, 2:31 PM -08:00

---
+++ js/sound.js
@@ -0,0 +1,9 @@
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    btnt = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText=sound
+
+    document.getElementById('buttons').appendChild(btn)
+})
