43fcfadc086cc819b78607eba8c6f3eead1fc63f
tylarsayers <143061860+tylarsayers@users.noreply.github.com>
2024-03-07 22:22:04 +0000

    Thu, Mar 7, 2024, 2:22 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,1 +1,20 @@
-loadText = document.querySelector('.loading)
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 30)
+
+
+function blurring() {
+    load++
+
+    if (load > 99){
+        clearinterval(int)
+    }
+    loadText.innerText = $ {
+            load
+        } %
+        loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
