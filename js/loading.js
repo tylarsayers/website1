f438062df8023767d73e9b3f2133797b31209d32
tylarsayers <143061860+tylarsayers@users.noreply.github.com>
2024-03-07 22:25:54 +0000

    Thu, Mar 7, 2024, 2:25 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -12,9 +12,8 @@ function blurring() {
     if (load > 99){
         clearinterval(int)
     }
-    loadText.innerText = $ {
-            load
-        } %
-        loadText.style.opacity = 1
-    bg.style.filter = "blur(10px)"
+    loadText.innerText = '${load}%'
+    loadText.style.opacity = 1 - load/100
+
+    bg.style.filter = 'blur(${30-(load/100)*30}p)
 }
