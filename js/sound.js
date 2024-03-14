8c18b2fda098d225c48a189a59190b5f0fdca04d
tylarsayers <143061860+tylarsayers@users.noreply.github.com>
2024-03-14 21:05:01 +0000

    Thu, Mar 14, 2024, 2:05 PM -07:00

--- js/sound.js
+++ js/sound.js
@@ -5,7 +5,7 @@ sounds.forEach(sound => {
     btn.classList.add('btn')
     btn.innerText=sound
     btn.addEventListener('click',() => {
-        document
+        document.getElementById(sound).play()
     })
     document.getElementById('buttons').appendChild(btn)
 })
